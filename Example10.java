bc240f10f172adcab79e5efbcb72784523e2c40c
GxuTan <gexutan@gmail.com>
2016-05-22 12:39:07 +0800

    a

---
+++ Example10.java
@@ -0,0 +1,15 @@
+package Button;
+
+public class Example10 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WindowGame winGame=new WindowGame();
+        winGame.setTitle("Guess number");
+
+	}
+
+}
